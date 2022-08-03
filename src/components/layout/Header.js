551049cb0af22e3b1ddb6654204ac8b5bff45c0a
sj5466 <108541042+sj5466@users.noreply.github.com>
2022-08-03 17:38:10 +0900

    2022/08/02

---
+++ src/components/layout/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+      <header id="header">
+        <div className="header__port">
+            <Link to="/portfolio">portfoilo</Link>
+        </div>
+        <div className="header__logo">
+            <Link to="/">songsuji</Link>
+        </div>
+        <nav className="header__menu">
+            <ul>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/reference">Refer</Link></li>
+                <li><Link to="/youtube">Youtube</Link></li>
+                <li><Link to="/movie">Movie</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
